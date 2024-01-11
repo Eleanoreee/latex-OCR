c6be9717b8b33d5727f52b82bb1af371e4e21f37
eleanoreee <2773107185@qq.com>
2024-01-11 17:58:24 +0800

    initial 2

---
+++ data/lst2txt.py
@@ -0,0 +1,16 @@
+# Specify paths
+lst_file_path = '/Users/eleanorewu/Latex-OCR/data/im2latex.lst'
+txt_file_path = '/Users/eleanorewu/Latex-OCR/data/im2latex.txt'
+
+# Read the .lst file and extract filenames
+with open(lst_file_path, 'r') as lst_file:
+    lines = lst_file.readlines()
+
+# Extract filenames from the lines
+filenames = [line.strip().split()[1] for line in lines]
+
+# Write filenames to the .txt file
+with open(txt_file_path, 'w') as txt_file:
+    txt_file.write('\n'.join(filenames))
+
+print(f"Conversion from {lst_file_path} to {txt_file_path} completed.")
